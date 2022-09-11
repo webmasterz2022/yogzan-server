176f10153c6bf616e71bc829bb3f05b39b249b85
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2022-09-12 03:24:46 +0700

    bugfix get home page images

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router()
-const multer = require('multer');
+// const multer = require('multer');
 const GalleryController = require('../controllers/galleryController')
-const { authentication } = require('../middlewares')
-const { getStorage } = require('../services/cloudinary');
+// const { authentication } = require('../middlewares')
+// const { getStorage } = require('../services/cloudinary');
 
-const maxSize = 2*1024*1024; //2 MB
+// const maxSize = 2*1024*1024; //2 MB
 
-const storage = type => getStorage(type);
-const upload = (type, fieldName) => multer({ 
-  storage: storage(type),
-  limits: { fileSize: maxSize }
-}).single(fieldName)
+// const storage = type => getStorage(type);
+// const upload = (type, fieldName) => multer({ 
+//   storage: storage(type),
+//   limits: { fileSize: maxSize }
+// }).single(fieldName)
 
-router.post('/upload', authentication, upload('Gallery', 'images'), GalleryController.upload)
-router.post('/upload-homepage', authentication, upload('Homepage', 'images'), GalleryController.uploadHomepage)
+// router.post('/upload', authentication, upload('Gallery', 'images'), GalleryController.upload)
+// router.post('/upload-homepage', authentication, upload('Homepage', 'images'), GalleryController.uploadHomepage)
 
 router.get('/homepage', GalleryController.findHomepage)
 router.get('/', GalleryController.findAll)
