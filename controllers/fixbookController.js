ccb1d9015bebaf6479271e4156d50e1ce0de98eb
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2022-11-01 16:41:40 +0800

    change sort get fixbooking list

--- controllers/fixbookController.js
+++ controllers/fixbookController.js
@@ -21,7 +21,7 @@ class FixBookController {
       .then(num => {
         totalData = num
         return FixBook.find()
-          .sort({date: 'desc', time: 'asc'})
+          .sort({date: 'desc'})
           .limit(perPage)
           .skip(perPage * (page-1))
       })
