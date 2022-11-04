865e1ee35e03752b81002cf8536407b6ee75aa5b
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2022-11-05 01:31:45 +0800

    add service update fixbook

--- routes/fixbookRoutes.js
+++ routes/fixbookRoutes.js
@@ -3,9 +3,9 @@ const FixBookController = require('../controllers/fixbookController')
 const { authentication } = require('../middlewares')
 
 router.post('/submit', FixBookController.submit)
+router.put('/:id', authentication, FixBookController.update)
 
 router.get('/', FixBookController.findAll)
 
-router.put('/:id', authentication, FixBookController.update)
 
 module.exports = router
