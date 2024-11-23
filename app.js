baed2546e9b8de5f873b93ae8d21b565fce6a37c
webmasterz2022 <webmasterz2022@gmail.com>
2024-11-23 02:00:00 +0000

    adjust from lightsail

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
